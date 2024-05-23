1580f4155419a7a35effaec76ebfb496b53173ad
Don Welch <dwelch@nait.ca>
2024-05-22 21:23:44 -0600

    Complete TDD demo for constructots

---
+++ OOPsSolution/OOPsReview/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ResidentAddress Address { get; set; }
+
+        public List<Employment> EmploymentPositions { get; set; }
+
+        public string FullName { get; }
+
+        public Person()
+        {
+            FirstName = "Unknown";
+            LastName = "Unknown";
+            EmploymentPositions = new List<Employment>();
+        }
+
+        public Person(string firstname, string lastname,
+                        ResidentAddress address, List<Employment> employmentpositions)
+        {
+            if (string.IsNullOrWhiteSpace(firstname))
+            {
+                throw new ArgumentNullException("First name is required");
+            }
+            if (string.IsNullOrWhiteSpace(lastname))
+            {
+                throw new ArgumentNullException("Last name is required");
+            }
+            FirstName = firstname;
+            LastName = lastname;
+            Address = address;
+            if (employmentpositions == null)
+            {
+                EmploymentPositions = new List<Employment>();
+            }
+            else
+            {
+                EmploymentPositions = employmentpositions;
+            }
+        }
+    }
+}
---
+++ OOPsSolution/TDDUnitTesting/Person_Should.cs
@@ -0,0 +1,155 @@
+using OOPsReview;
+using FluentAssertions;
+
+namespace TDDUnitTesting
+{
+    public class Person_Should
+    {
+        #region Constructors
+        [Fact]
+        public void Create_Instance_Using_Default_Constructor()
+        {
+            //Arrange (setup)
+            string expectedFirstName = "Unknown";
+            string expectedLastName = "Unknown";
+
+            //Act (the test action)
+            //sut subject under test
+            Person sut = new Person();
+
+            //Assert (checking)
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().BeNull();
+            sut.EmploymentPositions.Count().Should().Be(0);
+        }
+        [Fact]
+        public void Create_Instance_Using_Greedy_Constructor_Without_Address_And_Employments()
+        {
+            //Arrange (setup)
+            string expectedFirstName = "Lowand";
+            string expectedLastName = "Behold";
+
+            //Act (the test action)
+            //sut subject under test
+            Person sut = new Person(expectedFirstName, expectedLastName, null, null);
+
+            //Assert (checking)
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().BeNull();
+            sut.EmploymentPositions.Count().Should().Be(0);
+        }
+        [Fact]
+        public void Create_Instance_Using_Greedy_Constructor_With_Address_Without_Employments()
+        {
+            //Arrange (setup)
+            string expectedFirstName = "Lowand";
+            string expectedLastName = "Behold";
+            ResidentAddress expectedAdress = new ResidentAddress(12, "Maple St.",
+                                    "Edmonton", "AB", "T6Y7U8");
+
+            //Act (the test action)
+            //sut subject under test
+            Person sut = new Person(expectedFirstName, expectedLastName, expectedAdress, null);
+
+            //Assert (checking)
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().Be(expectedAdress);
+            sut.EmploymentPositions.Count().Should().Be(0);
+        }
+        [Fact]
+        public void Create_Instance_Using_Greedy_Constructor_With_Address_And_Employments()
+        {
+            //Arrange (setup)
+            string expectedFirstName = "Lowand";
+            string expectedLastName = "Behold";
+            ResidentAddress expectedAdress = new ResidentAddress(12, "Maple St.",
+                                    "Edmonton", "AB", "T6Y7U8");
+            Employment one = new Employment("Team Member", SupervisoryLevel.TeamMember,
+                                DateTime.Parse("2013/10/23"), 6.5);
+            Employment two = new Employment("Team Lead", SupervisoryLevel.TeamLeader,
+                                DateTime.Parse("2020/04/13"), 6.5);
+            List<Employment> expectedEmployments = new List<Employment>();
+            expectedEmployments.Add(one);
+            expectedEmployments.Add(two);
+
+            //Act (the test action)
+            //sut subject under test
+            Person sut = new Person(expectedFirstName, expectedLastName, 
+                        expectedAdress, expectedEmployments);
+
+            //Assert (checking)
+            sut.FirstName.Should().Be(expectedFirstName);
+            sut.LastName.Should().Be(expectedLastName);
+            sut.Address.Should().Be(expectedAdress);
+            sut.EmploymentPositions.Count().Should().Be(2);
+            sut.EmploymentPositions.Should().ContainInConsecutiveOrder(expectedEmployments);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("    ")]
+        public void Throw_Exception_Using_Greedy_Constructor_Missing_FirstName(string firstname)
+        {
+            //Arrange (setup)
+           // string expectedFirstName = "Lowand";
+            string expectedLastName = "Behold";
+            ResidentAddress expectedAdress = new ResidentAddress(12, "Maple St.",
+                                    "Edmonton", "AB", "T6Y7U8");
+            Employment one = new Employment("Team Member", SupervisoryLevel.TeamMember,
+                                DateTime.Parse("2013/10/23"), 6.5);
+            Employment two = new Employment("Team Lead", SupervisoryLevel.TeamLeader,
+                                DateTime.Parse("2020/04/13"), 6.5);
+            List<Employment> expectedEmployments = new List<Employment>();
+            expectedEmployments.Add(one);
+            expectedEmployments.Add(two);
+
+            //Act (the test action)
+            //capture the result of the action call
+            Action action = () => new Person(firstname, expectedLastName,
+                        expectedAdress, expectedEmployments);
+
+            //Assert (checking)
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("    ")]
+        public void Throw_Exception_Using_Greedy_Constructor_Missing_LastName(string lastname)
+        {
+            //Arrange (setup)
+            string expectedFirstName = "Lowand";
+            //string expectedLastName = "Behold";
+            ResidentAddress expectedAdress = new ResidentAddress(12, "Maple St.",
+                                    "Edmonton", "AB", "T6Y7U8");
+            Employment one = new Employment("Team Member", SupervisoryLevel.TeamMember,
+                                DateTime.Parse("2013/10/23"), 6.5);
+            Employment two = new Employment("Team Lead", SupervisoryLevel.TeamLeader,
+                                DateTime.Parse("2020/04/13"), 6.5);
+            List<Employment> expectedEmployments = new List<Employment>();
+            expectedEmployments.Add(one);
+            expectedEmployments.Add(two);
+
+            //Act (the test action)
+            //capture the result of the action call
+            Action action = () => new Person(expectedFirstName, lastname,
+                        expectedAdress, expectedEmployments);
+
+            //Assert (checking)
+            action.Should().Throw<ArgumentNullException>();
+        }
+        #endregion
+
+        #region Properties
+        public void Change_FirstName_Directly()
+        {
+
+        }
+        #endregion
+    }
+}
