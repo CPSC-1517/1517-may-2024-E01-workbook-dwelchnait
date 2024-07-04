ebbdb20aed59329e79bd257fb0f23f81d20f898f
Don Welch <dwelch@nait.ca>
2024-07-03 21:46:58 -0600

    complete web app to class library registration for DB via the extension class

--- CSSolution/WestWindApp/Program.cs
+++ CSSolution/WestWindApp/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using WestWindApp.Components;
+using WestWindSystem;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//retrieve the connection string from the appsettings.json
+//the connection string will be passed to the class library extension method
+//  for use in registering the access to the decidered database
+var connectstring = builder.Configuration.GetConnectionString("WWDB");
+
+//setup the registraton of services to be available for use by this web application
+//the technique used in this example has the registration encapsulated within the
+//  class library extension class
+//technically, you could do all the setup within this file
+builder.Services.WestWindExtensionServices(options => options.UseSqlServer(connectstring));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
---
+++ CSSolution/WestWindSystem/WestWindExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WestWindSystem.DAL;
+#endregion
+
+namespace WestWindSystem
+{
+    public static class WestWindExtensions
+    {
+        //this is a method of the extension class which will be call from the
+        //   Program.cs file of the Web Application
+        //this method will do 2 things
+        //  register the context connection string
+        //  register ALL services that I create within the BLL layer classes
+        public static void WestWindExtensionServices(this IServiceCollection services,
+            Action<DbContextOptionsBuilder>options)
+        {
+            //handle the connection string
+            //add my contexgt class to the services (IServiceCollection)
+            services.AddDbContext<WestWindContext>(options);
+        }
+    }
+}
